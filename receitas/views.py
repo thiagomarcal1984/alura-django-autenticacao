a441266e7ba505d30e49a471ab62871bf5a8d2c4
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-06-24 21:45:11 -0300

    Cadastro e login.

--- receitas/views.py
+++ receitas/views.py
@@ -7,7 +7,7 @@ def index(request):
     dados = {
         'receitas' : receitas
     }
-    return render(request,'index.html', dados)
+    return render(request, 'index.html', dados)
 
 def receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
@@ -16,7 +16,7 @@ def receita(request, receita_id):
         'receita' : receita
     }
 
-    return render(request,'receita.html', receita_a_exibir)
+    return render(request, 'receita.html', receita_a_exibir)
 
 def buscar(request):
     lista_receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
