d837333880f30490b647807a1da3dfc58335e086
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-06-25 14:12:36 -0300

    Login e dashboard.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('cadastro', views.cadastro, name='cadastro'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
